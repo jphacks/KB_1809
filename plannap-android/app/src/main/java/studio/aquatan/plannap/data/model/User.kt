537866da911cb7952836439fd7d1054e9fe69e55
shanpu <cheztomo513@gmail.com>
2018-10-21 00:50:01 +0900

    Userモデル修正

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/model/User.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/model/User.kt
@@ -1,11 +1,14 @@
 package studio.aquatan.plannap.data.model
 
 import androidx.recyclerview.widget.DiffUtil
+import com.squareup.moshi.Json
 
 data class User(
-    val id: Long,
-    val name: String,
-    val pass: String
+    @Json(name = "pk") val id: Long,
+    val email: String,
+    @Json(name = "created_at") val createdDate: String,
+    @Json(name = "icon") val iconUrl: String,
+    @Json(name = "update_at") val updateDate: String
 ) {
     companion object {
         val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Plan>() {
