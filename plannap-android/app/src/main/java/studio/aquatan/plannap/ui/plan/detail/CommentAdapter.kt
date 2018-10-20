0d78f3e159ced2988ab0f4471be3e0116254f1ee
ken_kentan <ken.kentan.k2@gmail.com>
2018-10-21 04:11:57 +0900

    add pin mipmap (#39)
    * update PlanDetailActivity
    
    * add pin mipmap

--- plannap-android/app/src/main/java/studio/aquatan/plannap/ui/plan/detail/CommentAdapter.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/ui/plan/detail/CommentAdapter.kt
@@ -1,11 +1,13 @@
 package studio.aquatan.plannap.ui.plan.detail
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import studio.aquatan.plannap.R
 import studio.aquatan.plannap.data.model.Comment
+import studio.aquatan.plannap.databinding.ItemCommentBinding
 
 class CommentAdapter(
     private val layoutInflater: LayoutInflater
@@ -18,14 +20,21 @@ class CommentAdapter(
     override fun getItemId(position: Int) = getItem(position).id
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val binding: ItemCommentBinding =
+                DataBindingUtil.inflate(layoutInflater, R.layout.item_comment, parent, false)
+
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(getItem(position))
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    inner class ViewHolder(
+        private val binding: ItemCommentBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(comment: Comment) {
+            binding.data = comment
+        }
     }
 }
--- plannap-android/app/src/main/java/studio/aquatan/plannap/util/DataBindingHelper.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/util/DataBindingHelper.kt
@@ -1,10 +1,37 @@
 package studio.aquatan.plannap.util
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import studio.aquatan.plannap.R
 
 @BindingAdapter("isVisible")
 fun setIsVisible(view: View, isVisible: Boolean) {
     view.isVisible = isVisible
+}
+
+@BindingAdapter("price")
+fun setMoney(view: TextView, price: Int) {
+    view.apply {
+        text = context.getString(R.string.text_price, price)
+    }
+}
+
+@BindingAdapter("duration")
+fun setDuration(view: TextView, duration: Int) {
+    view.apply {
+        text = context.getString(R.string.text_duration, duration)
+    }
+}
+
+@BindingAdapter("srcUrl")
+fun setSrcUrl(view: ImageView, url: String?) {
+    url ?: return
+
+    Glide.with(view.context)
+        .load(url)
+        .into(view)
 }
