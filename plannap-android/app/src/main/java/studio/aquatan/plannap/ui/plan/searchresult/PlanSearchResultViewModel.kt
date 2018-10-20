5a533ca011d2ba907c16be89854df3f3e9450b70
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-21 04:39:24 +0900

    導線接続

--- plannap-android/app/src/main/java/studio/aquatan/plannap/ui/plan/searchresult/PlanSearchResultViewModel.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/ui/plan/searchresult/PlanSearchResultViewModel.kt
@@ -1,9 +1,12 @@
 package studio.aquatan.plannap.ui.plan.searchresult
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import studio.aquatan.plannap.data.PlanRepository
+import studio.aquatan.plannap.data.model.Plan
+import studio.aquatan.plannap.ui.SingleLiveEvent
 
 class PlanSearchResultViewModel(
     private val planRepository: PlanRepository
@@ -13,16 +16,17 @@ class PlanSearchResultViewModel(
 
     val title = MutableLiveData<String>()
 
-    val planList = Transformations.switchMap(keyword) {
+    val planList: LiveData<List<Plan>> = Transformations.switchMap(keyword) {
         title.value = it
         return@switchMap planRepository.getPlanListByKeyword(it)
     }
+    val startPlanDetailActivity = SingleLiveEvent<Long>()
 
     fun onActivityCreated(areaName: String?) {
         keyword.value = areaName
     }
 
     fun onPlanClick(id: Long) {
-
+        startPlanDetailActivity.value = id
     }
 }
