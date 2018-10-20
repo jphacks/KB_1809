3755e3f90996366125193ed98fc2d95b1b7a5c5a
shanpu <cheztomo513@gmail.com>
2018-10-20 15:32:15 +0900

    SpotServiceの実装

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/SpotService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/SpotService.kt
@@ -1,17 +1,14 @@
 package studio.aquatan.plannap.data.api
 
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 import studio.aquatan.plannap.data.model.Spot
 
 interface SpotService {
-    @GET("/api/v1/spots")
-    fun getSpots(): Call<List<Spot>>
+    @GET("/api/v1/spots/")
+    fun getSpots(@Query("planId") planId: Int): Call<List<Spot>>
 
     @Headers("Accept: application/json", "Content-Type: application/json")
     @POST("/api/v1/spot")
-    fun postSpot(@Body body: HashMap<String, String>): Call<String>
+    fun postSpot(@Body spot: Spot): Call<Spot>
 }
