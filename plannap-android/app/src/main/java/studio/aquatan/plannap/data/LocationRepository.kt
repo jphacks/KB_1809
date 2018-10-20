e22ea56c05a308433728e7a114181d8c8e3e52c6
shanpu <30888767+chez-shanpu@users.noreply.github.com>
2018-10-20 17:45:05 +0900

    Location関連のAPIの実装 (#16)

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/LocationRepository.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/LocationRepository.kt
@@ -1,9 +1,9 @@
 package studio.aquatan.plannap.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.experimental.GlobalScope
-import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -19,32 +19,16 @@ class LocationRepository {
 
     private val service = retrofit.create(LocationService::class.java)
 
-    fun getLocationList(): LiveData<List<Location>> {
-        val result = MutableLiveData<List<Location>>()
-
-        GlobalScope.launch {
-            // TODO fetch Location list via API
-            delay(2000)
-//            result.postValue(DUMMY_LIST)
-
-//            try {
-//                val response = service.plans().execute()
-//                result.postValue(response.body())
-//            } catch (e: Exception) {
-//                Log.e(javaClass.simpleName, "Failed to fetch plans", e)
-//            }
-        }
-
-        return result
-    }
-
-    fun getLocationById(id: Long): LiveData<Location> {
+    fun getLocationList(locationId: Long): LiveData<Location> {
         val result = MutableLiveData<Location>()
 
         GlobalScope.launch {
-            // TODO fetch Location via API
-            delay(1000)
-//            result.postValue(DUMMY_LIST.find { it.id == id })
+            try {
+                val response = service.getLocation(locationId).execute()
+                result.postValue(response.body())
+            } catch (e: Exception) {
+                Log.e(javaClass.simpleName, "Failed to fetch getLocation", e)
+            }
         }
 
         return result
