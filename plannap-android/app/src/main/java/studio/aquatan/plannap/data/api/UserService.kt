9e992204d39ac62a56ea558c131b2e4d2677b664
shanpu <cheztomo513@gmail.com>
2018-10-20 17:58:30 +0900

    UserServise,UserRepositoryの実装

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/UserService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/UserService.kt
@@ -1,17 +1,14 @@
 package studio.aquatan.plannap.data.api
 
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 import studio.aquatan.plannap.data.model.User
 
 interface UserService {
-    @GET("/api/v1/users")
-    fun getUsers(): Call<List<User>>
+    @GET("/api/v1/users/{id}")
+    fun getUser(@Path("id") userId: Long): Call<User>
 
     @Headers("Accept: application/json", "Content-Type: application/json")
     @POST("/api/v1/user")
-    fun postUser(@Body body: HashMap<String, String>): Call<String>
+    fun postUser(@Body user: User): Call<User>
 }
