703425e7b83b0f3c11d6f3da7174ab7e70cc6fc5
shanpu <cheztomo513@gmail.com>
2018-10-20 17:41:21 +0900

    LocationServise,LocationRepositoryの実装

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/LocationService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/LocationService.kt
@@ -1,17 +1,13 @@
 package studio.aquatan.plannap.data.api
 
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.Path
 import studio.aquatan.plannap.data.model.Location
 
 interface LocationService {
-    @GET("/api/v1/locations")
-    fun getLocations(): Call<List<Location>>
 
-    @Headers("Accept: application/json", "Content-Type: application/json")
-    @POST("/api/v1/location")
-    fun postLocation(@Body body: HashMap<String, String>): Call<String>
+    @GET("/api/v1/location/{id}")
+    fun getLocation(@Path("id") locationId: Long): Call<Location>
+
 }
