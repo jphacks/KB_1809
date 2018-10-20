74f970b68108e509d44d6af5403a811321c2748f
ken_kentan <ken.kentan.k2@gmail.com>
2018-10-20 19:23:37 +0900

    update PlanSearchResultActivity (#23)

--- plannap-android/app/src/main/java/studio/aquatan/plannap/ui/plan/searchresult/PlanSearchResultViewModel.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/ui/plan/searchresult/PlanSearchResultViewModel.kt
@@ -1,6 +1,7 @@
 package studio.aquatan.plannap.ui.plan.searchresult
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import studio.aquatan.plannap.data.PlanRepository
 
@@ -8,9 +9,20 @@ class PlanSearchResultViewModel(
     private val planRepository: PlanRepository
 ) : ViewModel() {
 
+    private val keyword = MutableLiveData<String>()
+
     val title = MutableLiveData<String>()
 
+    val planList = Transformations.switchMap(keyword) {
+        title.value = it
+        return@switchMap planRepository.getPlanByKeyword(it)
+    }
+
     fun onActivityCreated(areaName: String?) {
-        title.value = areaName
+        keyword.value = areaName
+    }
+
+    fun onPlanClick(id: Long) {
+
     }
 }
