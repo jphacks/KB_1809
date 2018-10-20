bed2e0e3bc0998c2e5bbe46c285a7ddf8d98ea63
shanpu <cheztomo513@gmail.com>
2018-10-20 15:54:36 +0900

    CommentServise,CommentRepositoryの実装

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/CommentService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/CommentService.kt
@@ -1,17 +1,14 @@
 package studio.aquatan.plannap.data.api
 
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 import studio.aquatan.plannap.data.model.Comment
 
 interface CommentService {
-    @GET("/api/v1/comments")
-    fun getComments(): Call<List<Comment>>
+    @GET("/api/v1/comments/")
+    fun getComments(@Path("planId") planId: Long): Call<List<Comment>>
 
     @Headers("Accept: application/json", "Content-Type: application/json")
     @POST("/api/v1/comment")
-    fun postComment(@Body body: HashMap<String, String>): Call<String>
+    fun postComment(@Body comment: Comment): Call<Comment>
 }
