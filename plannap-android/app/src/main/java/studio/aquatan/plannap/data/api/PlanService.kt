54ef7b84cd94bed0cce5433dff6603f810d6eda3
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-24 01:18:38 +0900

    認証情報更新

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
@@ -7,15 +7,15 @@ import studio.aquatan.plannap.data.model.PostPlan
 
 interface PlanService {
     //現在，トークンはハードコーディング
-    @Headers("Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDAzMDY5MTIsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDA0NzcxMn0.67rGu3zwo88AQutMdEpdMlWokn6j_ymGrf1eX-t0Y1A")
+    @Headers("Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDA1NjY2MjEsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDMwNzQyMX0.Af_dX3BzQzDSPxLvHtYxLY5UjZf4HfhSfuiC7yy5fwQ")
     @GET("/plan/plans/")
     fun getPlans(): Call<List<Plan>>
 
-    @Headers("Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDAzMDY5MTIsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDA0NzcxMn0.67rGu3zwo88AQutMdEpdMlWokn6j_ymGrf1eX-t0Y1A")
+    @Headers("Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDA1NjY2MjEsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDMwNzQyMX0.Af_dX3BzQzDSPxLvHtYxLY5UjZf4HfhSfuiC7yy5fwQ")
     @GET("/plan/plans/{id}")
     fun getPlan(@Path("id") planId: Long): Call<Plan>
 
-    @Headers("Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDAzMDY5MTIsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDA0NzcxMn0.67rGu3zwo88AQutMdEpdMlWokn6j_ymGrf1eX-t0Y1A")
+    @Headers("Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDA1NjY2MjEsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDMwNzQyMX0.Af_dX3BzQzDSPxLvHtYxLY5UjZf4HfhSfuiC7yy5fwQ")
     @GET("/plan/plans/")
     fun getPlan(@Query("location") location: String): Call<List<Plan>>
 
