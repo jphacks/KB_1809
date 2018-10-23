3150d61d6085bd71cb8489e8a774fd709883b4ed
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-24 00:36:23 +0900

    認証情報を更新

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
@@ -22,12 +22,8 @@ interface PlanService {
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json",
-        "Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDAzMDY5MTIsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDA0NzcxMn0.67rGu3zwo88AQutMdEpdMlWokn6j_ymGrf1eX-t0Y1A"
+        "Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImRldmVsb3AiLCJleHAiOjE1NDA1NjY2MjEsImVtYWlsIjoiaG9nZUBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTU0MDMwNzQyMX0.Af_dX3BzQzDSPxLvHtYxLY5UjZf4HfhSfuiC7yy5fwQ"
     )
     @POST("/plan/plans/")
-    fun postPlan(
-//        @Body json: JSONObject
-//        @Body json: String
-        @Body body: PostPlan
-    ): Call<String>
+    fun postPlan(@Body body: PostPlan): Call<String>
 }
