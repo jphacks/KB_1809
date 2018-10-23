cd8bd542064330ca1594ae4c8f6c18569010427d
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-24 00:03:10 +0900

    update Plan model

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/model/Plan.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/model/Plan.kt
@@ -5,29 +5,25 @@ import com.squareup.moshi.Json
 
 data class Plan(
     @Json(name = "pk") val id: Long,
-    val name: String,
-    val price: Int,
-    val duration: Int,
-    val note: String,
-    val location: Location,
-    @Json(name = "reports") val reportList: List<Report> = emptyList(),
-    @Json(name = "favorites") val favoriteList: List<Favorite> = emptyList(),
+    @Json(name = "name") val name: String,
+    @Json(name = "note") val note: String,
+    @Json(name = "price") val price: Int,
+    @Json(name = "duration") val duration: Int,
     @Json(name = "created_at") val date: String,
-    @Json(name = "comments") val commentList: List<Comment> = emptyList(),
-    @Json(name = "spots") val spotList: List<Spot>,
-    val user: User,
+    @Json(name = "location") val location: Location,
     @Json(name = "favorite_count") val favoriteCount: Int,
-    @Json(name = "comment_count") val commentCount: Int
-    ) {
+    @Json(name = "comment_count") val commentCount: Int,
+    @Json(name = "spots") val spotList: List<Spot> = emptyList(),
+    @Json(name = "favorites") val favoriteList: List<Favorite> = emptyList(),
+    @Json(name = "reports") val reportList: List<Report> = emptyList(),
+    @Json(name = "comments") val commentList: List<Comment> = emptyList(),
+    @Json(name = "user") val user: User,
+    @Json(name = "is_favorite") val isFavorite: Boolean
+) {
     companion object {
         val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Plan>() {
-            override fun areItemsTheSame(oldItem: Plan, newItem: Plan): Boolean {
-                return oldItem.id == newItem.id
-            }
-
-            override fun areContentsTheSame(oldItem: Plan, newItem: Plan): Boolean {
-                return oldItem == newItem
-            }
+            override fun areItemsTheSame(oldItem: Plan, newItem: Plan) = oldItem.id == newItem.id
+            override fun areContentsTheSame(oldItem: Plan, newItem: Plan) = oldItem == newItem
         }
     }
 }
