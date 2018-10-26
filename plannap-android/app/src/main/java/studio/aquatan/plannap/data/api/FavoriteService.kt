03682c8187dbbde68ca6252ce824db2f429dd839
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-26 14:07:10 +0900

    update FavoriteService

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/FavoriteService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/FavoriteService.kt
@@ -3,6 +3,7 @@ package studio.aquatan.plannap.data.api
 import retrofit2.Call
 import retrofit2.http.*
 import studio.aquatan.plannap.data.model.Favorite
+import studio.aquatan.plannap.data.model.PostFavorite
 
 interface FavoriteService {
     @GET("/plan/favorite/")
@@ -11,7 +12,9 @@ interface FavoriteService {
     @GET("/plan/favorite/")
     fun getFavoriteByUserid(@Path("userId") userId: Long): Call<Favorite>
 
-    @Headers("Accept: application/json", "Content-Type: application/json")
-    @POST("/plan/favorite")
-    fun postFavorite(@Body favorite: Favorite): Call<Favorite>
+    @POST("/plan/favs/")
+    fun postFavorite(@Body favorite: PostFavorite): Call<Favorite>
+
+    @HTTP(method = "DELETE", path = "/plan/favs/", hasBody = true)
+    fun deleteFavorite(@Body favorite: PostFavorite): Call<Favorite>
 }
