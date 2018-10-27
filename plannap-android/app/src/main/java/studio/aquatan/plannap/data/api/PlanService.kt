40b5d545a810957c54e87f2edd9fd7f89beaaecb
Pudding <s.kokuryo@gmail.com>
2018-10-27 18:24:40 +0900

    fix redundant slash

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
@@ -7,19 +7,19 @@ import studio.aquatan.plannap.data.model.PostPlan
 
 interface PlanService {
 
-    @GET("/api/v1//plans/")
+    @GET("/api/v1/plans/")
     fun getPlans(): Call<List<Plan>>
 
     @GET("/api/v1/plans/{id}/")
     fun getPlan(@Path("id") planId: Long): Call<Plan>
 
-    @GET("/api/v1//plans/")
+    @GET("/api/v1/plans/")
     fun getPlan(@Query("location") location: String): Call<List<Plan>>
 
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json"
     )
-    @POST("/api/v1//plans/")
+    @POST("/api/v1/plans/")
     fun postPlan(@Body body: PostPlan): Call<Plan>
 }
