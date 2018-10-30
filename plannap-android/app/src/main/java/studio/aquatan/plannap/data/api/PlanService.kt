303dc7bca0e5de9f41622d3bb0857814c824c7e2
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-30 14:27:20 +0900

    update PlanService

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/PlanService.kt
@@ -15,6 +15,9 @@ interface PlanService {
     @GET("/api/v1/plans/")
     fun getPlans(): Call<List<Plan>>
 
+    @GET("/api/v2/plans/?limit=5")
+    fun getPlans(@Query("offset") page: Int): Call<List<Plan>>
+
     @GET("/api/v1/plans/{id}/")
     fun getPlan(@Path("id") planId: Long): Call<Plan>
 
