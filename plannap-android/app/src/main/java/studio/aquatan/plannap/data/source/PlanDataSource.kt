17a3d016dfc5e8dfe461831a57f84f57f642cb63
ken-kentan <ken.kentan.k2@gmail.com>
2018-10-30 23:03:47 +0900

    update PlanDataSource

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/source/PlanDataSource.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/source/PlanDataSource.kt
@@ -1,41 +1,58 @@
 package studio.aquatan.plannap.data.source
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
-import kotlinx.coroutines.experimental.GlobalScope
-import kotlinx.coroutines.experimental.launch
+import studio.aquatan.plannap.data.NetworkState
 import studio.aquatan.plannap.data.api.PlanService
+import studio.aquatan.plannap.data.model.Page
 import studio.aquatan.plannap.data.model.Plan
 
 class PlanDataSource(
     private val service: PlanService
-) : PageKeyedDataSource<Int, Plan>() {
+) : PageKeyedDataSource<String, Plan>() {
 
     companion object {
         private const val TAG = "PlanDataSource"
     }
 
-    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Plan>) {
-        GlobalScope.launch {
-            try {
-                val response = service.getPlans(0).execute()
-                callback.onResult(response.body() ?: emptyList(), null, 1)
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to fetch plans", e)
-            }
+    val networkState = MutableLiveData<NetworkState>()
+
+    override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<String, Plan>) {
+        networkState.postValue(NetworkState.LOADING)
+
+        try {
+            val response = service.getPlans(null).execute()
+            val page = response.body() ?: Page()
+
+            networkState.postValue(NetworkState.LOADED)
+
+            callback.onResult(page.resultList, null, page.nextKey)
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to load plans", e)
+
+            val error = NetworkState.error(e.message ?: "unknown error")
+            networkState.postValue(error)
         }
     }
 
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Plan>) {
-        GlobalScope.launch {
-            try {
-                val response = service.getPlans(params.key).execute()
-                callback.onResult(response.body() ?: emptyList(), params.key + 1)
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to fetch plans", e)
-            }
+    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<String, Plan>) {
+        networkState.postValue(NetworkState.LOADING)
+
+        try {
+            val response = service.getPlans(params.key).execute()
+            val page = response.body() ?: Page()
+
+            networkState.postValue(NetworkState.LOADED)
+
+            callback.onResult(page.resultList, page.nextKey)
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to load plans", e)
+
+            val error = NetworkState.error(e.message ?: "unknown error")
+            networkState.postValue(error)
         }
     }
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Plan>) {}
+    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<String, Plan>) {}
 }
--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/source/PlanDataSourceFactory.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/source/PlanDataSourceFactory.kt
@@ -1,14 +1,19 @@
 package studio.aquatan.plannap.data.source
 
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.DataSource
 import studio.aquatan.plannap.data.api.PlanService
 import studio.aquatan.plannap.data.model.Plan
 
 class PlanDataSourceFactory(
     private val service: PlanService
-) : DataSource.Factory<Int, Plan>() {
+) : DataSource.Factory<String, Plan>() {
 
-    override fun create(): DataSource<Int, Plan> {
-        return PlanDataSource(service)
+    val sourceLiveData = MutableLiveData<PlanDataSource>()
+
+    override fun create(): DataSource<String, Plan> {
+        val source = PlanDataSource(service)
+        sourceLiveData.postValue(source)
+        return source
     }
 }
