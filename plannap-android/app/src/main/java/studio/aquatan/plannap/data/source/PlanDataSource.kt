46974ee281c0feb18b008aec8b5120d5063a88a3
ken-kentan <ken.kentan.k2@gmail.com>
2018-11-02 21:49:04 +0900

    update PlanDataSource

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/source/PlanDataSource.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/source/PlanDataSource.kt
@@ -3,6 +3,8 @@ package studio.aquatan.plannap.data.source
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.launch
 import studio.aquatan.plannap.data.NetworkState
 import studio.aquatan.plannap.data.api.PlanService
 import studio.aquatan.plannap.data.model.Page
@@ -16,22 +18,39 @@ class PlanDataSource(
         private const val TAG = "PlanDataSource"
     }
 
+    private var retry: (() -> Unit)? = null
+
+    val initialLoad = MutableLiveData<NetworkState>()
     val networkState = MutableLiveData<NetworkState>()
 
+    fun retryAllFailed() {
+        val prevRetry = retry
+        retry = null
+        prevRetry?.let {
+            GlobalScope.launch { it.invoke() }
+        }
+    }
+
     override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<String, Plan>) {
+        initialLoad.postValue(NetworkState.LOADING)
         networkState.postValue(NetworkState.LOADING)
 
         try {
             val response = service.getPlans(null).execute()
             val page = response.body() ?: Page()
 
+            retry = null
+            initialLoad.postValue(NetworkState.LOADED)
             networkState.postValue(NetworkState.LOADED)
 
             callback.onResult(page.resultList, null, page.nextKey)
         } catch (e: Exception) {
-            Log.e(TAG, "Failed to load plans", e)
+            Log.e(TAG, "loadInitial() failed", e)
+
+            retry = { loadInitial(params, callback) }
 
             val error = NetworkState.error(e.message ?: "unknown error")
+            initialLoad.postValue(error)
             networkState.postValue(error)
         }
     }
@@ -43,11 +62,14 @@ class PlanDataSource(
             val response = service.getPlans(params.key).execute()
             val page = response.body() ?: Page()
 
+            retry = null
             networkState.postValue(NetworkState.LOADED)
 
             callback.onResult(page.resultList, page.nextKey)
         } catch (e: Exception) {
-            Log.e(TAG, "Failed to load plans", e)
+            Log.e(TAG, "loadAfter() failed", e)
+
+            retry = { loadAfter(params, callback) }
 
             val error = NetworkState.error(e.message ?: "unknown error")
             networkState.postValue(error)
