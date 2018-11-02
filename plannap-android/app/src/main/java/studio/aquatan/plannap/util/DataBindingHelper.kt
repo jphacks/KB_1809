1345cc9cb49d9fda75b206ed93a840ac07b65921
ken-kentan <ken.kentan.k2@gmail.com>
2018-11-02 22:31:00 +0900

    support unixtime response

--- plannap-android/app/src/main/java/studio/aquatan/plannap/util/DataBindingHelper.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/util/DataBindingHelper.kt
@@ -7,6 +7,10 @@ import androidx.databinding.BindingAdapter
 import com.facebook.drawee.view.SimpleDraweeView
 import com.google.android.material.textfield.TextInputLayout
 import studio.aquatan.plannap.R
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.LazyThreadSafetyMode.NONE
 
 @BindingAdapter("isVisible")
 fun setIsVisible(view: View, isVisible: Boolean) {
@@ -24,6 +28,17 @@ fun setDuration(view: TextView, duration: Int) {
 fun setCost(view: TextView, cost: Int) {
     view.apply {
         text = context.getString(R.string.text_price, cost)
+    }
+}
+
+private val DATE_FORMAT by lazy(NONE) { SimpleDateFormat("yyyy/MM/dd hh:mm:ss", Locale.JAPAN) }
+
+@BindingAdapter("date")
+fun setDate(view: TextView, date: Date?) {
+    view.text = if (date != null) {
+        DATE_FORMAT.format(date)
+    } else {
+        null
     }
 }
 
