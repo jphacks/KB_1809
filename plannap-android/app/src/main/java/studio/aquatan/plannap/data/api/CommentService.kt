db384cb0e0f75ad59502fbadc584b9eed298130a
ken-kentan <ken.kentan.k2@gmail.com>
2018-11-04 23:19:22 +0900

    support paging

--- plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/CommentService.kt
+++ plannap-android/app/src/main/java/studio/aquatan/plannap/data/api/CommentService.kt
@@ -6,12 +6,19 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
+import studio.aquatan.plannap.data.CommentRepository.Companion.PAGE_SIZE
 import studio.aquatan.plannap.data.model.Comment
+import studio.aquatan.plannap.data.model.Page
 import studio.aquatan.plannap.data.model.PostComment
 
 interface CommentService {
-    @GET("/api/v1/plans/{id}/comments/")
-    fun getCommentsByPlanId(@Path("id") planId: Long): Call<List<Comment>>
+
+    @GET("/api/v2/plans/{id}/comments/?limit=$PAGE_SIZE")
+    fun getComments(
+        @Path("id") planId: Long,
+        @Query("cursor") key: String?
+    ): Call<Page<Comment>>
 
     @Headers("Accept: application/json", "Content-Type: application/json")
     @POST("/api/v1/plans/{id}/comments/")
